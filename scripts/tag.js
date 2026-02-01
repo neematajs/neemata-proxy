59094545ef0fb53a967bd2002c5cd5bd7ad44759
Denis Ilchishin <ilchishin.denis@gmail.com>
2026-02-01 21:44:28 +0700

    update publish wf

---
+++ scripts/tag.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { parseArgs } from 'node:util'
+
+const { positionals } = parseArgs({ allowPositionals: true })
+const [version] = positionals
+let tag = 'latest'
+const tags = { alpha: /alpha/, beta: /beta/, rc: /rc/ }
+for (const [tagName, regex] of Object.entries(tags)) {
+  if (regex.test(version)) {
+    tag = tagName
+    break
+  }
+}
+process.stdout.write(tag)
