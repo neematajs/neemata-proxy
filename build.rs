dd7919752c5170c7b4ac5fcdd5d5544015fcf98a
Denis Ilchishin <ilchishin.denis@gmail.com>
2026-02-07 16:45:12 +0700

    fix: patch pingora graceful shutdown extra timeout

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
+    println!("cargo:rerun-if-changed=Cargo.toml");
+    println!("cargo:rerun-if-changed=patches");
+    patch_crate::run().expect("Failed while patching");
     napi_build::setup();
 }
